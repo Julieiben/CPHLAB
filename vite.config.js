f2e53ca44d9dab8301f611339a2375996986d795
nick kaastrup <nick490j@stud.kea.dk>
2022-12-20 23:27:16 +0100

    nick push

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        fremkaldelse: resolve(__dirname, "src/fremkaldelse.html"),
+        analogguide: resolve(__dirname, "src/analogguide.html"),
+        singleview: resolve(__dirname, "src/singleview.html"),
+        smalfilmoverspillet: resolve(__dirname, "src/smalfilmoverspillet.html"),
+
+
+
+
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },
